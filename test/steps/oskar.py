b16e9ce8f5721da0b8eab7ee97da46343935bb3e
Iain Emsley <iain.emsley@kellogg.ox.ac.uk>
2016-01-20 17:57:56 +0000

    System package test installed

--- test/steps/oskar.py
+++ test/steps/oskar.py
@@ -1,8 +1,13 @@
 from behave import given, when, then
 import os
 
+import steps.docker
+
 dirname = "/home/iain/git/docker/oskar/"
 fname = "Dockerfile"
+imagename = "oskar"
+package = "oskar"
+
 @given(u'that I have a build directory "{directory}"')
 def step_impl(context, directory):
     pass
@@ -13,4 +18,17 @@ def step_impl(context, dfile):
 
 @then(u'I have the base installation file')
 def step_impl(context):
-    assert os.path.isfile(dirname + fname) is True    
+    assert os.path.isfile(dirname + fname) is True  
+
+@given(u'that I have a Docker image called "{imagename}"')
+def step_impl(context, imagename):
+    pass
+
+@when(u'I have installed the "{package}" package')
+def step_impl(context, package):
+    pass
+
+@then(u'I can query the image to see that it is loaded')
+def step_impl(context):
+    bdd = steps.docker.BDDDocker(imagename)
+    assert bdd.system_package(package) is True
