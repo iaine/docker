33fe5df5d1769abea5df79f04ad3bbe48d775f34
Iain Emsley <iain.emsley@kellogg.ox.ac.uk>
2016-08-26 11:16:37 +0100

    Added file writing for results

--- tools/dockerparse.py
+++ tools/dockerparse.py
@@ -82,11 +82,26 @@ def token_rules(token, depth=1):
     token_rules(token[depth:], depth)
 
 if __name__ == '__main__':
+    '''
+       Takes the docker file as an argument
+       Evaluates the file and parses data to a log file
+       If it cannot evaluate, creates an error file that is also used to end
+       end the process
+    '''
+     
     fname = sys.argv[1]
     try:
+        evaluation = None
         with open(fname, 'rb') as f:
             data = f.read()
-            print(eval_token(read_from_tokens(tokenize(data))))
+            evaluation = eval_token(read_from_tokens(tokenize(data)))
+            f.write(evaluation)
         f.closed
+
+        with open(fname + "_log.txt", 'wb') as fi:
+            fi.write(evaluation)
+        fi.closed
     except Exception as e:
-        printf("parsing exception %s"%e)
+        with open("error.txt", "wb") as fh:
+            fh.write(e)
+        fh.closed
