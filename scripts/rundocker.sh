c5ed22fe2a065d50f6d33d5081aa3a555eda2dc1
iaine <iain.emsley@kellogg.ox.ac.uk>
2016-09-14 14:36:27 +0100

    Update rundocker.sh
    Added description of file

--- scripts/rundocker.sh
+++ scripts/rundocker.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+################
+#
+#  Script to run a docker image and retrieve its PID
+#
+#  author: Iain Emsley
+#
+################
+
 PROCNAME=$1
 
 NCID=$(docker run -d ${PROCNAME})
