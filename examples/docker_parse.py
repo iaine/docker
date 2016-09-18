52d8a41d2d40b1b86ca1a4252b669e57ac76a1d6
Iain <iain.emsley@kellogg.ox.ac.uk>
2016-09-18 22:53:50 +0100

    Updated the example

--- examples/docker_parse.py
+++ examples/docker_parse.py
@@ -1,24 +1,10 @@
 import sys    
-'''
-       Takes the docker file as an argument
-       Evaluates the file and parses data to a log file
-       If it cannot evaluate, creates an error file that is also used to end
-       end the process
-    '''
+from hawser import Hawser
 
-    fname = sys.argv[1]
-    try:
-        evaluation = None
-        with open(fname, 'rb') as f:
-            data = f.read()
-            evaluation = eval_token(read_from_tokens(tokenize(data)))
-            f.write(evaluation)
-        f.closed
+file_name = sys.argv[1]
 
-        with open(fname + "_log.txt", 'wb') as fi:
-            fi.write(evaluation)
-        fi.closed
-    except Exception as e:
-        with open("error.txt", "wb") as fh:
-            fh.write(e)
-        fh.closed
+if not file_name:
+    print "No filename given"
+    sys.exit(1)
+
+hawser.Hawser(file_name)
