bb10edefe57719127868712d9f2143e7e6f341a8
Iain Emsley <iain.emsley@kellogg.ox.ac.uk>
2016-10-10 16:19:27 +0100

    Corrected typo

--- docker-hawser/testenv.py
+++ docker-hawser/testenv.py
@@ -2,6 +2,6 @@ from nose.tools import assert_true, assert_false
 
 from operating_sys import Container_Environment
 
-def test_found_Dokcer_version():
+def test_found_Docker_version():
     _dock = Container_Environment().get_docker_version
     assert_true(_dock, '1.12.1')
