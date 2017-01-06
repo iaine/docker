c4027cd74d75495c0505da5d708fc8268475472b
Iain Emsley <iain.emsley@kellogg.ox.ac.uk>
2017-01-06 15:38:44 +0000

    Docker client data added

--- hawser/container.py
+++ hawser/container.py
@@ -1,12 +1,33 @@
+import subprocess
+
 class Container_Environment():
     '''
        Get the Docker information
     '''
-
+    def __init__ (self):
+        _dock = subprocess.check_output(["docker", "version"]).decode('utf8')
+        d = _dock.split('\n\n')
+        self.client={}
+        dock = d[0].split('\n')
+        for i in dock[1:]:
+            _tmp = i.split(':')
+            self.client[_tmp[0].strip()] = str(_tmp[1].strip())
 
     def get_docker_version(self):
         '''
-          Get the Dokcer version
+          Get the Docker version
         '''
-        return '1.12.1'
+        return self.client['Version']
 
+    def get_docker_api(self):
+        return self.client['API version']
+
+
+    def get_docker_go(self):
+        return self.client['Go version']
+
+    def get_docker_git(self):
+        return self.client['Git commit']
+
+    def get_docker_os(self):
+        return self.client['OS/Arch']
